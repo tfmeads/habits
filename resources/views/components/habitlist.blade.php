1e8c31ca81a53d275ba884573401886707e68a78
Taylor <taylormeads1@gmail.com>
2025-01-16 20:10:32 -0800

    added padding to homepage header

--- resources/views/components/habitlist.blade.php
+++ resources/views/components/habitlist.blade.php
@@ -2,7 +2,7 @@
 if(!isset($type)){ $type = 'info';}
 ?>
 <div style="display: flex; flex-direction: column; align-items : center;">
-    <h3>{{ $title }}</h3>
+    <h3 style="width: max-content; padding: 20px;">{{ $title }}</h3>
     <ul>
         @foreach ($list as $habit)
             <li style="">@include('components.habit-item-'.$type, ['habit' => $habit])
