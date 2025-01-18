46abc9d1d01059657973ecb2b8ce684c06499146
Taylor <taylormeads1@gmail.com>
2025-01-17 20:52:28 -0800

    blake recommended defaults

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,7 +4,10 @@ namespace App\Providers;
 
 use App\Models\User;
 use App\Models\Habit;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\Facades\Gate;
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\ServiceProvider;
 use Whitecube\LaravelTimezones\Facades\Timezone;
 
@@ -25,6 +28,15 @@ class AppServiceProvider extends ServiceProvider
     {
         Timezone::set('America/Los_Angeles'); //TODO set this dynamically when user logs in
 
+        Model::shouldBeStrict(! $this->app->isProduction());
+        Model::unguard();
+
+        //Can't make this work on my local test environment - breaks CSS even when I force the asset to load  with {{asset('css/home.css',true)}}
+        if($this->app->isProduction()){
+            URL::forceScheme('https');
+        }
+
+        DB::prohibitDestructiveCommands($this->app->isProduction());
 
         Gate::define('edit-habit', function(User $user, Habit $habit){
             return $habit->user->is($user);
